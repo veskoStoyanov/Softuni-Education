bb655cacb8c0cb47ac1abec4067fe782e0cb9672
veskoStoyanov <snaikeee@mail.bg>
2020-05-01 13:56:38 +0300

    deleted old react project

--- ReactJS/Motors.bg-Express-Mongo/src/infrastructore/hocs/index.js
+++
@@ -1,4 +0,0 @@
-export {default as withAuthAdmin} from './withAuthAdmin';
-export {default as withAuthLogged} from './withAuthLogged';
-export {default as withGuests} from './withGuests';
-export {default as withUserOnly} from './withUserOnly';
